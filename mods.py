c08ca1bb435b15e020411cb411b4b26063494d27
Garrett Haima <garretthaima@gmail.com>
2021-03-24 09:31:04 -0400

    adding debug print statements

--- mods.py
+++ mods.py
@@ -8,13 +8,14 @@ def download_mod(mod_id, path):
     steamcmd = ["steamcmd"]
     steamcmd.extend(["+login", os.getenv["STEAM_USER"], os.getenv["STEAM_PASS"]])
     # steamcmd.extend(["+login", os.environ["STEAM_USER"]])
-    steamcmd.extend(["+force_install_dir", path])
+    # steamcmd.extend(["+force_install_dir", path])
     steamcmd.extend(["+workshop_download_item", "107410 " + mod_id])
     steamcmd.extend(["validate", "+quit"])
-    print(steamcmd)
+    print("Downloading mod")
     subprocess.run(steamcmd)
 
 def create_symlink(source, dest):
+    print("Creating symlink")
     cmd = ["ln"]
     cmd.extend(["-s"])
     cmd.extend([source])
