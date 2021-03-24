e337593d1a106f04717539900364267291f08794
Garrett Haima <garretthaima@gmail.com>
2021-03-24 09:45:06 -0400

    added print to help add the mods to the cfg

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ root = os.getenv("ARMA_ROOT") # "/games/arma/serverfiles"
 steam_path = os.getenv("STEAM_ROOT") #"/games/arma/steamcmd/steamapps/workshop/content/107410/"
 
 mod_list = modlist("arma_mods.html")
-
+mod_string = ""
 for mod in mod_list:
     download_mod(mod[1], root + "mods")
     create_symlink(steam_path + mod[1], root + "/mods/" + mod[0])
+    mod_string = mod_string + "mods/\\" + mod[0] + ";"
+
+print(mod_string)
